4b02536b92f09bfa0df3d1f7dda96fd54a6891c0
sonali patwardhan <sonalipatwardhan2017@gmail.com>
2020-05-26 20:39:06 +0530

    Copy Of React Application With Json Server

---
+++ json-server/public/precache-manifest.7c449160ecd4c712db69872ca333b8ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7166f45c6b10ded2fe55d0582b0685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8491c4529f6b89bd8a",
+    "url": "/static/css/2.37c8d227.chunk.css"
+  },
+  {
+    "revision": "99366db05d6a57961349",
+    "url": "/static/css/main.401ce26c.chunk.css"
+  },
+  {
+    "revision": "3e8491c4529f6b89bd8a",
+    "url": "/static/js/2.ffc4dca1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.ffc4dca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99366db05d6a57961349",
+    "url": "/static/js/main.8ad63114.chunk.js"
+  },
+  {
+    "revision": "1be0497b9e4f0f67eec1",
+    "url": "/static/js/runtime-main.817cd467.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
